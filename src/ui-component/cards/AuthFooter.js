2b810a58360d93744491c270fd3bf6484aa6602c
phucleit <phucleit95@gmail.com>
2023-12-25 15:15:54 +0700

    footer

--- src/ui-component/cards/AuthFooter.js
+++ src/ui-component/cards/AuthFooter.js
@@ -1,15 +1,13 @@
 // material-ui
 import { Link, Typography, Stack } from '@mui/material';
 
-// ==============================|| FOOTER - AUTHENTICATION 2 & 3 ||============================== //
-
 const AuthFooter = () => (
   <Stack direction="row" justifyContent="space-between">
     <Typography variant="subtitle2" component={Link} href="https://itvungtau.com/" target="_blank" underline="hover">
-      Thiết kế Website Vũng Tàu
+      &copy; Thiết kế Website Vũng Tàu
     </Typography>
     <Typography variant="subtitle2" component={Link} href="http://thietkewebvungtau.net/" target="_blank" underline="hover">
-      Thiết kế Website bởi IT Vũng Tàu
+      &copy; Thiết kế Website bởi IT Vũng Tàu
     </Typography>
   </Stack>
 );
