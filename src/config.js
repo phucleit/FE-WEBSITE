cedc6d550b6346bdcfb247d8ad7906cdb7fc98f3
phucleit <phucleit95@gmail.com>
2025-04-01 09:59:51 +0700

    initial

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const config = {
   borderRadius: 12,
   // API_URL: 'http://localhost:8500/v1',
   // API_UPLOAD: 'http://localhost:8500'
-  API_URL: 'https://webdms.itvungtau.com.vn/v1',
-  API_UPLOAD: 'https://webdms.itvungtau.com.vn'
+  API_URL: 'https://bewebdms.itvungtau.com.vn/v1',
+  API_UPLOAD: 'https://bewebdms.itvungtau.com.vn'
   // API_URL: 'https://backendtest.thietkewebvungtau.com/v1',
   // API_UPLOAD: 'https://backendtest.thietkewebvungtau.com'
 };
