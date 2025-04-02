0dd5045a848134f0d3b64028a37e4c7731db6b63
phucleit <phucleit95@gmail.com>
2025-04-02 16:10:26 +0700

    sua config

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ const config = {
   defaultPath: '',
   fontFamily: `'Roboto', sans-serif`,
   borderRadius: 12,
-  API_URL: 'http://localhost:8500/v1',
-  API_UPLOAD: 'http://localhost:8500'
+  // API_URL: 'http://localhost:8500/v1',
+  // API_UPLOAD: 'http://localhost:8500'
   // API_URL: 'https://bewebdms.itvungtau.com.vn/v1',
   // API_UPLOAD: 'https://bewebdms.itvungtau.com.vn'
-  // API_URL: 'https://backendtest.thietkewebvungtau.com/v1',
-  // API_UPLOAD: 'https://backendtest.thietkewebvungtau.com'
+  API_URL: 'https://backendtest.thietkewebvungtau.com/v1',
+  API_UPLOAD: 'https://backendtest.thietkewebvungtau.com'
 };
 
 export default config;
